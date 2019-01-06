b142b1e197c46f4166f8c54b8b937c02b2873bb0
Nevaan Perera <nevaan9@gmail.com>
2019-01-06 13:37:06 -0500

    Integrated back and frontend

--- server/index.js
+++ server/index.js
@@ -12,6 +12,18 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use('/api/posts', posts);
 
+// Handle Production
+// Point to which public folder is our entry point
+if (process.env.NODE_ENV === 'production') {
+    // Use the public folder created via npm run build
+    app.use(express.static(__dirname + '/public/'));
+
+    // Handle Single Page Applications
+    app.use(/.*/, (req, res) => {
+        res.sendFile(__dirname + '/public/index.html');
+    });
+}
+
 app.listen(port, () => {
     console.log(`Server running on ${port}`);
 });
